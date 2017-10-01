880403fbd3b505abb1c710d43aa92f92f5ef1b8c
jhonnold <jayhonnold@gmail.com>
2017-10-01 10:58:10 -0400

    Added Foundation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button, Link } from 'react-foundation';
 import logo from './logo.svg';
 import './App.scss';
 
 export default () => (
   <div className="App">
+    <Button>Save</Button>
+    <Link>Test</Link>
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h1 className="App-title">Welcome to React</h1>
